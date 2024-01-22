71f483fdcfab0b684024620defc0a37f5ae04b1a
Martin Odstrcilik <martin.odstrcilik@lablabs.io>
2024-01-22 16:34:06 +0100

    feat: bump chart version to 0.8.1

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "helm_chart_name" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "0.7.0"
+  default     = "0.8.1"
   description = "Version of the Helm chart"
 }
 
